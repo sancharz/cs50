34a19eecbc94a094ef5daf69939b8ec9b30c900d
alana davitt <alana@alana-davitts-macbook.local>
2016-11-30 14:07:08 -0500

    Initial Commit

---
+++ forget me not/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  forget me not
+//
+//  Created by alana davitt on 11/30/16.
+//  Copyright © 2016 cs50 project team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
